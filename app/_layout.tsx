5340a95e3745b090fc9ec5bf69587401c95dc958
Nzagha David Eze <33941830+zaghadon@users.noreply.github.com>
2025-03-11 03:05:14 -0700

    Chore: Update configuration files and add assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
+import * as SplashScreen from 'expo-splash-screen';
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SweetStateProvider } from '@/store/sweetState/Provider';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,12 +32,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SweetStateProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SweetStateProvider>
   );
 }
