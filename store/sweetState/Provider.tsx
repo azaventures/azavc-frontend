ce0d7d3c831172fe788177eeb7514d46916a505c
Nzagha David Eze <33941830+zaghadon@users.noreply.github.com>
2025-06-18 07:04:03 -0700

    Fix StoreProvider import in Provider.tsx

--- store/sweetState/Provider.tsx
+++ store/sweetState/Provider.tsx
@@ -1,7 +1,7 @@
 import { AuthStore } from './auth';
 import React from 'react';
 import { UIStore } from './ui';
-import StoreProvider from 'react-sweet-state';
+import { StoreProvider } from 'react-sweet-state';
 
 // Provider component to wrap the app with all stores
 export function SweetStateProvider({ children }: { children: React.ReactNode }) {
